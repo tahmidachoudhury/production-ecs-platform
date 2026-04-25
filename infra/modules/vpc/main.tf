0bc269811dcb975af5d346ccfad3347b4053f1cc
Tahmid Choudhury <tahmidachoudhury@outlook.com>
2026-04-25 19:29:09 +0100

    add: private and public subnet infra

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -9,3 +9,46 @@ resource "aws_vpc" "main" {
     Environment = var.environment
   }
 }
+
+resource "aws_subnet" "public-1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-west-2a"
+
+  tags = {
+    Name = "${var.project_name}-public-subnet-1"
+    Environment = var.environment
+  }
+}
+resource "aws_subnet" "public-2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-west-2b"
+
+  tags = {
+    Name = "${var.project_name}-public-subnet-2"
+    Environment = var.environment
+  }
+}
+resource "aws_subnet" "private-1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.10.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-west-2a"
+  tags = {
+    Name = "${var.project_name}-private-subnet-1"
+    Environment = var.environment
+  }
+}
+resource "aws_subnet" "private-2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.11.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-west-2b"
+  tags = {
+    Name = "${var.project_name}-private-subnet-2"
+    Environment = var.environment
+  }
+}

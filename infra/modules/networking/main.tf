dd80fdec0b8bbab9846112d19a1e1e0f2a0b4856
Tahmid Choudhury <tahmidachoudhury@outlook.com>
2026-04-25 20:22:21 +0100

    add: public route table

--- infra/modules/networking/main.tf
+++ infra/modules/networking/main.tf
@@ -33,3 +33,17 @@ resource "aws_subnet" "private" {
     Environment = var.environment
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.example.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+    Environment = var.environment
+  }
+}

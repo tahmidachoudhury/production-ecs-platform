5888b8f47b85b29993a7beeba05b333b2b2e0387
Tahmid Choudhury <tahmidachoudhury@outlook.com>
2026-04-25 19:43:44 +0100

    add: pre-commit terraform hooks

--- infra/modules/networking/main.tf
+++ infra/modules/networking/main.tf
@@ -1,35 +1,35 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-  enable_dns_support = true
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "${var.project_name}-vpc"
+    Name        = "${var.project_name}-vpc"
     Environment = var.environment
   }
 }
 
 resource "aws_subnet" "public" {
-  count = length(var.public_subnet_cidr)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnet_cidr[count.index]
+  count                   = length(var.public_subnet_cidr)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr[count.index]
   map_public_ip_on_launch = true
-  availability_zone = var.availability_zone[count.index]
+  availability_zone       = var.availability_zone[count.index]
 
   tags = {
-    Name = "${var.project_name}-public-subnet-${count.index + 1}"
+    Name        = "${var.project_name}-public-subnet-${count.index + 1}"
     Environment = var.environment
   }
 }
 
 resource "aws_subnet" "private" {
-  count = length(var.private_subnet_cidr)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnet_cidr[count.index]
+  count             = length(var.private_subnet_cidr)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr[count.index]
   availability_zone = var.availability_zone[count.index]
   tags = {
-    Name = "${var.project_name}-private-subnet-${count.index + 1}"
+    Name        = "${var.project_name}-private-subnet-${count.index + 1}"
     Environment = var.environment
   }
 }

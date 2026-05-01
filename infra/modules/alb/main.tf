e27b8253e951db02af7510f5ca89cb905138eb3d
Tahmid Choudhury <tahmidachoudhury@outlook.com>
2026-05-01 08:52:38 +0100

    add: alb listener complete for HTTP

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -1,4 +1,4 @@
-resource "aws_lb" "umami-app" {
+resource "aws_lb" "app" {
   name                       = "${var.project_name}-alb"
   internal                   = false
   load_balancer_type         = "application"
@@ -11,7 +11,7 @@ resource "aws_lb" "umami-app" {
   }
 }
 
-resource "aws_lb_target_group" "umami-app" {
+resource "aws_lb_target_group" "app" {
   name        = "${var.project_name}-alb-tg"
   port        = 3000
   protocol    = "HTTP"
@@ -24,3 +24,27 @@ resource "aws_lb_target_group" "umami-app" {
     protocol = "HTTP"
   }
 }
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
+
+# resource "aws_lb_listener" "https" {
+#   load_balancer_arn = aws_lb.main.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+#   certificate_arn   = var.certificate_arn
+
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.app.arn
+#   }
+# }

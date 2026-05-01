c12f6ad63a5156cdcd81e5e5dfe2061abce510bc
Tahmid Choudhury <tahmidachoudhury@outlook.com>
2026-05-01 12:20:42 +0100

    chore: ecs module initiated

---
+++ infra/modules/ecs/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+  type        = string
+  description = "The name of the project, used for resource naming and tagging."
+}
+
+variable "environment" {
+  type        = string
+  description = "The deployment environment (dev or prod)"
+}

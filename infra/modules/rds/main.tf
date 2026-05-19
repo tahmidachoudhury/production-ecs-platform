2b95f4c5c66a22b8ab59ea6bb03861fb379b0c74
Tahmid Choudhury <tahmidachoudhury@outlook.com>
2026-05-19 20:10:16 +0100

    refactor: rds secret handling

--- infra/modules/rds/main.tf
+++ infra/modules/rds/main.tf
@@ -9,17 +9,17 @@ resource "aws_db_subnet_group" "private" {
 }
 
 resource "aws_db_instance" "psql" {
-  allocated_storage      = var.storage_size
-  storage_type           = var.storage_type
-  engine                 = var.engine
-  engine_version         = var.engine_version
-  instance_class         = var.instance
-  db_subnet_group_name   = aws_db_subnet_group.private.name
-  db_name                = "mydb"
-  username               = "postgres"
-  password               = "ibabdbobdowbodbw"
-  skip_final_snapshot    = true
-  vpc_security_group_ids = [var.rds_sg_id]
+  allocated_storage           = var.storage_size
+  storage_type                = var.storage_type
+  engine                      = var.engine
+  engine_version              = var.engine_version
+  instance_class              = var.instance
+  db_subnet_group_name        = aws_db_subnet_group.private.name
+  db_name                     = "mydb"
+  username                    = "postgres"
+  manage_master_user_password = true
+  skip_final_snapshot         = true
+  vpc_security_group_ids      = [var.rds_sg_id]
 
   tags = {
     Name        = "${var.project_name}-rds-db"
--- infra/modules/rds/outputs.tf
+++ infra/modules/rds/outputs.tf
@@ -13,7 +13,13 @@ output "endpoint" {
   description = "The DB connection endpoint in `address:port` format."
 }
 
-output "db_secret" {
-  value       = aws_db_instance.psql.password
-  description = "Password for the master DB user."
+output "master_user_secret_arn" {
+  value       = aws_db_instance.psql.master_user_secret[0].secret_arn
+  description = "ARN of the Secrets Manager secret containing RDS master credentials."
+  sensitive   = true
+}
+
+output "db_name" {
+  value       = aws_db_instance.psql.db_name
+  description = "The name of the default database."
 }

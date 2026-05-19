57b0efa3b9d5149e58d4483e838f66a3ab39cb3b
Tahmid Choudhury <tahmidachoudhury@outlook.com>
2026-05-19 20:07:42 +0100

    refactor: iam and ecs module for security rds handling

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,3 @@
-module "iam" {
-  source       = "./modules/iam"
-  project_name = var.project_name
-  environment  = var.environment
-}
-
 module "networking" {
   source       = "./modules/networking"
   project_name = var.project_name
@@ -46,6 +40,36 @@ module "rds" {
   rds_sg_id          = module.security_groups.rds_sg_id
 }
 
+module "iam" {
+  source        = "./modules/iam"
+  project_name  = var.project_name
+  environment   = var.environment
+  db_secret_arn = module.rds.master_user_secret_arn
+}
+
+module "ecs" {
+  source                    = "./modules/ecs"
+  project_name              = var.project_name
+  environment               = var.environment
+  aws_region                = var.aws_region
+  ecs_sg_id                 = module.security_groups.ecs_sg_id
+  private_subnet_ids        = module.networking.private_subnet_ids
+  container_name            = local.container_name
+  container_image           = var.container_image
+  alb_target_group_arn      = module.alb.target_group_arn
+  task_role_arn             = module.iam.ecs_task_role_arn
+  execution_role_arn        = module.iam.execution_role_arn
+  cloudwatch_log_group_name = module.cloudwatch.log_group_name
+  cpu                       = var.cpu
+  memory                    = var.memory
+  container_port            = var.container_port
+  number_of_tasks           = var.number_of_tasks
+  db_secret_arn             = module.rds.master_user_secret_arn
+  db_host                   = module.rds.address
+  db_port                   = module.rds.port
+  db_name                   = module.rds.db_name
+}
+
 module "acm" {
   source                  = "./modules/acm"
   project_name            = var.project_name
@@ -66,27 +90,4 @@ module "dns" {
   alb_dns_name              = module.alb.alb_dns_name
   alb_zone_id               = module.alb.alb_zone_id
   domain_validation_options = module.acm.domain_validation_options
-}
-
-module "ecs" {
-  source                    = "./modules/ecs"
-  project_name              = var.project_name
-  environment               = var.environment
-  aws_region                = var.aws_region
-  ecs_sg_id                 = module.security_groups.ecs_sg_id
-  private_subnet_ids        = module.networking.private_subnet_ids
-  container_name            = local.container_name
-  container_image           = var.container_image
-  alb_target_group_arn      = module.alb.target_group_arn
-  task_role_arn             = module.iam.ecs_task_role_arn
-  execution_role_arn        = module.iam.execution_role_arn
-  cloudwatch_log_group_name = module.cloudwatch.log_group_name
-  cpu                       = var.cpu
-  memory                    = var.memory
-  container_port            = var.container_port
-  number_of_tasks           = var.number_of_tasks
-  depends_on                = [module.rds]
-  rds_port                  = module.rds.port
-  rds_address               = module.rds.address
-  rds_db_secret             = module.rds.db_secret
 }
